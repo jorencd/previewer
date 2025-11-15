f664cb47c1c2f5e05850c043d85a32021bc2cf7b
Jorence Mendoza <jorencemendoza2@gmail.com>
2025-11-15 15:04:07 +0800

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { marked } from 'marked';
 
 function App() {
+  const [inputText, setInputText] = useState(`# Welcome to Markdown Previewer!
+  
+## Sub-heading
+
+This is a [link](https://www.freecodecamp.org/)
+
+Inline \`code\` and block of code:
+
+\`\`\`
+function helloWorld() {
+  console.log("Hello, world!");
+}
+\`\`\`
+
+- List item 1
+- List item 2
+
+> Blockquote
+
+![Image](https://via.placeholder.com/150)
+
+**Bolded text**
+`);
+
+  const handleChange = (event) => {
+    setInputText(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="editor-container">
+          <h3>Editor</h3>
+          <textarea
+            id="editor"
+            value={inputText}
+            onChange={handleChange}
+            rows="10"
+            cols="50"
+          />
+        </div>
+        <div className="preview-container">
+          <h3>Preview</h3>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{
+              __html: marked(inputText),
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
